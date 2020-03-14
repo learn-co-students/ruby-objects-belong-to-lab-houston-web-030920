7593a324596aeda2810d6a3ef3228dfd2d7f72de
isaac-3 <isaacgc0596@gmail.com>
2020-03-13 22:35:40 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,11 @@
+# require_relative('./author.rb')
+
+class Post
+    attr_accessor :title, :author
+    # def author=(author)
+    #     @author = author
+    # end
+    # def author
+    #     return @author
+    # end
+end
